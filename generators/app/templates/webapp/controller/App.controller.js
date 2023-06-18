f451806753cf27625de70031e04509bc0b949c9c
Peter Muessig <peter.muessig@sap.com>
2023-06-18 12:33:33 +0200

    feat: adopted to latest best practices + tests

--- generators/app/templates/webapp/controller/App.controller.js
+++ generators/app/templates/webapp/controller/App.controller.js
@@ -1,13 +1,10 @@
-sap.ui.define([
-  "./BaseController"
-], function (BaseController) {
-  "use strict";
+sap.ui.define(["./BaseController"], function (BaseController) {
+	"use strict";
 
-  return BaseController.extend("<%= appId %>.controller.App", {
-    onInit: function() {
-      // apply content density mode to root view
-      this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-    }
-  });
-
+	return BaseController.extend("<%= appId %>.controller.App", {
+		onInit: function () {
+			// apply content density mode to root view
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		}
+	});
 });
