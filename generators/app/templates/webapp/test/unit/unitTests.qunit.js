3db45bb8b5e2742e5d7f7b03924e0b49e563e0c7
Matthias Osswald <mat.osswald@sap.com>
2024-10-18 14:26:53 +0200

    feat: Use Test Starter
    This change introduces the test starter as a replacement for the
    traditional test suite + test page setup.
    
    The feature is only available for application projects starting with
    UI5 1.124, so the existing test setup is kept for older projects.

--- generators/app/templates/webapp/test/unit/unitTests.qunit.js
+++ generators/app/templates/webapp/test/unit/unitTests.qunit.js
@@ -1,12 +1,1 @@
-/* global QUnit */
-
-// https://api.qunitjs.com/config/autostart/
-QUnit.config.autostart = false;
-
-sap.ui.getCore().attachInit(function () {
-	"use strict";
-
-	sap.ui.require(["unit/controller/App.qunit"], function () {
-		QUnit.start();
-	});
-});
+sap.ui.define(["./controller/App.qunit"]);
