363e10f1eabe89cfc3a0262204c04a7fa0eeadab
Andreas Kunz <andreas.kunz@sap.com>
2025-09-25 10:23:21 +0200

    fix: theme fix for older versions; correct test name; also update template dependencies, remove unneeded method from template (#19)
    * chore: update dependencies, remove unneeded method from template
    
    * fix: theme fix for older versions; correct test name
    
    * chore: update dependencies
    
    * chore: tweak .gitignore

--- generators/app/templates/webapp/test-lt1_124/unit/controller/Main.qunit.js
+++ generators/app/templates/webapp/test-lt1_124/unit/controller/Main.qunit.js
@@ -2,9 +2,9 @@
 sap.ui.define(["<%= appURI %>/controller/Main.controller"], function (MainController) {
 	"use strict";
 
-	QUnit.module("Sample App controller test");
+	QUnit.module("Sample Main controller test");
 
-	QUnit.test("The AppController class has a sayHello method", function (assert) {
+	QUnit.test("The MainController class has a sayHello method", function (assert) {
 		// as a very basic test example just check the presence of the "sayHello" method
 		assert.strictEqual(typeof MainController.prototype.sayHello, "function");
 	});
